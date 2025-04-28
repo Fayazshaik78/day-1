8b371643033f639198fdbae789bd1f97f335d078
Fayaz github <shaikfayazbaba321@gmail.com>
2025-04-28 19:50:03 +0530

    Add files via upload

---
+++ Library.java
@@ -0,0 +1,13 @@
+public class Library {
+    public static  void displayWelcomeMessage(){
+        System.out.println("Welcome to the Library Management System!");
+
+    }
+public static void main (String [] args){
+    displayWelcomeMessage();
+}
+
+
+
+    
+}
