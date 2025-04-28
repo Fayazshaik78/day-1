39cd677a09116918fe4cf4547386adf0bf32d868
Fayaz github <shaikfayazbaba321@gmail.com>
2025-04-28 19:48:59 +0530

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,12 @@
+public class Company {
+    public static void  printCompanyPolicy(){
+        System.out.println("Company Policy: All employees must adhere to company policies regarding conduct,\r\n" + //
+                        "confidentiality, and professionalism.");
+
+    }
+    public static void main (String [] args){
+        printCompanyPolicy();
+
+    }
+    
+}
